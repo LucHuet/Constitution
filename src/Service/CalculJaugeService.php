803bf1b0f2235226e1bcc6a658aa40c86f840ce1
Luc <luck.huet@gmail.com>
2018-10-05 21:20:00 +0200

    premieres étapes de la création de la jauge

---
+++ src/Service/CalculJaugeService.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Repository\ActeurPartieRepository;
+use App\Repository\PouvoirPartieRepository;
+use App\Entity\Partie;
+use App\Entity\ActeurPartie;
+
+class CalculJaugeService
+{
+
+
+    function __construct()
+    {
+
+    }
+
+    public function ajoutActeur()
+    {
+
+    }
+
+    public function ajoutDesignation()
+    {
+
+    }
+
+    public function ajoutPouvoir()
+    {
+
+    }
+
+    public function ajoutCondition()
+    {
+
+    }
+
+
+
+}
