2037859291376c677312accfea4a8e5759dfaf05
Luc <luck.huet@gmail.com>
2018-11-24 10:15:27 +0100

    Suppression de React DnD

--- assets/js/acteur_react.js
+++ assets/js/acteur_react.js
@@ -2,24 +2,13 @@ import React from 'react';
 //render permet de transformer un template en page html réelle
 import { render } from 'react-dom';
 import ActeurApp from './Acteur/ActeurApp';
-import Board from './Board/Board';
-import { observe } from './Board/Game';
 
 const shouldShowProut = true;
 
-const root = document.getElementById('board-app');
-
-observe(knightPosition =>
-  render(
-    <Board knightPosition={knightPosition} />,
-    root
-  )
+render(
+  <ActeurApp
+    withProut = {shouldShowProut}
+    {...window.ACTEUR_APP_PROPS}
+  />,
+  document.getElementById('acteur-app')
 );
-
-//render(
-//  <ActeurApp
-//    withProut = {shouldShowProut}
-//    {...window.ACTEUR_APP_PROPS}
-//  />,
-//  document.getElementById('acteur-app')
-//);
